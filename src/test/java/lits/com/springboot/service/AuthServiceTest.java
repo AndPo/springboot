dba534528a24784f60ab98b0cf4c90c5d8ba89dc
aecktann <serhiiilnytskyi@gmail.com>
2019-06-25 12:23:26 +0300

    Added Tests to UserService

--- src/test/java/lits/com/springboot/service/AuthServiceTest.java
+++ src/test/java/lits/com/springboot/service/AuthServiceTest.java
@@ -1,12 +1,17 @@
 package lits.com.springboot.service;
 
+import lits.com.springboot.model.User;
 import lits.com.springboot.repository.UserRepository;
+import lits.com.springboot.service.impl.AuthServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.authentication.AuthenticationManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AuthServiceTest {
@@ -21,10 +26,16 @@ public class AuthServiceTest {
     private UserRepository userRepository;
 
     @InjectMocks
-    private AuthService authService;
+    private AuthService authService = new AuthServiceImpl();
 
     @Test
     public void shouldAuth() {
+        User user  = new User();
+        user.setId(1L);
 
+        when(userRepository.findOneByEmail("login")).thenReturn(user);
+        when(tokenService.createToken(user.getId())).thenReturn("BarrerToken");
+
+        assertEquals("BarrerToken", authService.auth("login", "pass"));
     }
 }
--- src/test/java/lits/com/springboot/service/UserServiceTest.java
+++ src/test/java/lits/com/springboot/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package lits.com.springboot.service;
 
+import lits.com.springboot.model.Role;
+import lits.com.springboot.model.User;
 import lits.com.springboot.repository.UserRepository;
 import lits.com.springboot.service.impl.UserServiceImpl;
 import org.junit.Test;
@@ -7,6 +9,19 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -15,16 +30,46 @@ public class UserServiceTest {
     private UserRepository userRepository;
 
     @InjectMocks
-    UserService userService = new UserServiceImpl();
+    UserServiceImpl userService = new UserServiceImpl();
 
-    @Test
+    @Test(expected = UsernameNotFoundException.class)
     public void shouldLoadUserByUserName() {
+        User user = new User();
+        user.setEmail("UserName");
+        user.setPassword("Pass");
 
+        when(userRepository.findOneByEmail("UserName")).thenReturn(user);
+
+        assertNotNull(userService.loadUserByUsername("UserName"));
+        verify(userRepository).findOneByEmail("UserName");
+
+        userService.loadUserByUsername("UserName2");
     }
 
     @Test
     public void shouldGetAuthotity() {
+        User user = new User();
+        Set<Role> roles = new HashSet<>();
+        List<GrantedAuthority> authorities = new ArrayList<>();
 
+        Role role = new Role();
+        role.setName("ROLE");
+        roles.add(role);
+
+        user.setRoles(roles);
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+
+        assertEquals(authorities, userService.getAuthority(user));
+    }
+
+    @Test
+    public void shouldGetUserById() {
+        User user = new User();
+
+        when(userRepository.findById(1L)).thenReturn(user);
+
+        assertEquals(user, userService.findById(1L));
+        verify(userRepository).findById(1L);
     }
 
 }
